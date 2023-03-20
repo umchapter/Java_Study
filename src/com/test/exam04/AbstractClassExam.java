1c5a803f70efe01ec8d92254b56063359d785276
Jihoon An <u.m.chapter@gmail.com>
2023-03-20 15:10:12 +0900

    first commit

---
+++ src/com/test/exam04/AbstractClassExam.java
@@ -0,0 +1,18 @@
+package com.test.exam04;
+
+public class AbstractClassExam {
+    
+    public static void main(String[] args) {
+        
+    }
+}
+
+abstract class SalesPaln {
+
+    public void companyGoal(){
+        System.out.println("2023년 전사 목표 300억 달성");
+    }
+
+    abstract public void departGoal(); // 추상 메소드
+}
+
