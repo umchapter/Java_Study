ac8384de5e47a3cb46c21737b7aabca2fb0398ae
Jihoon An <u.m.chapter@gmail.com>
2023-03-20 15:24:28 +0900

    Changes to be committed: 	modified:   bin/com/test/exam04/Animal.class 	new file:   bin/com/test/exam04/AnimalMove.class 	modified:   bin/com/test/exam04/Eagle.class 	modified:   bin/com/test/exam04/Human.class 	modified:   bin/com/test/exam04/PolymorphismExam02.class 	modified:   bin/com/test/exam04/Tiger.class 	modified:   src/com/test/exam04/PolymorphismExam02.java

--- src/com/test/exam04/PolymorphismExam02.java
+++ src/com/test/exam04/PolymorphismExam02.java
@@ -1,40 +1,52 @@
 package com.test.exam04;
 
-class Animal {
-    public void move() { // 추상 메소드기 때문에 Animal 클래스 안의 추상 메소드를 없애면 아래 코드 작동 안됨.
-        System.out.println("동물이 움직입니다.");
+interface Animal {
+    public void move();  // 추상 메소드기 때문에 Animal 클래스 안의 추상 메소드를 없애면 아래 코드 작동 안됨.
+        // System.out.println("동물이 움직입니다.");
     }
-}
 
-class Human extends Animal {
+class Human implements Animal {
     public void move() {
         System.out.println("사람이 두 발로 걷습니다.");
     }
 }
 
-class Tiger extends Animal {
+class Tiger implements Animal {
     public void move() {
         System.out.println("호랑이가 네 발로 뜁니다.");
     }
 }
 
-class Eagle extends Animal {
+class Eagle implements Animal {
     public void move() {
         System.out.println("독수리가 하늘을 납니다.");
     }
 }
 
-public class PolymorphismExam02 {
-    public static void main(String[] args) {
-        PolymorphismExam02 animalTest = new PolymorphismExam02(); // animalTest는 PE02 객체
-            animalTest.moveAnimal(new Human()); // Human 인자 받음.
-            animalTest.moveAnimal(new Tiger());
-            animalTest.moveAnimal(new Eagle());
-    }
-    public void moveAnimal(Animal animal) {
-        /* PE02 객체 내에서 moveanimal이라는 메소드 생성
-         * 인자(arguments)로 Animal 클래스의 종속 개체 받음.
-         */
+class AnimalMove {
+    Animal animal;
+    public AnimalMove(Animal animal) {
+        this.animal = animal;
         animal.move();
     }
+}
+
+public class PolymorphismExam02 {
+    public static void main(String[] args) {
+
+        new AnimalMove(new Human());
+        new AnimalMove(new Tiger());
+        new AnimalMove(new Eagle());
+
+        // PolymorphismExam02 animalTest = new PolymorphismExam02(); // animalTest는 PE02 객체
+        //     animalTest.moveAnimal(new Human()); // Human 인자 받음.
+        //     animalTest.moveAnimal(new Tiger());
+        //     animalTest.moveAnimal(new Eagle());
+    }
+    // public void moveAnimal(Animal animal) {
+    //     /* PE02 객체 내에서 moveanimal이라는 메소드 생성
+    //      * 인자(arguments)로 Animal 클래스의 종속 개체 받음.
+    //      */
+    //     animal.move();
+    // }
 }
